70749c491cb76e296683824ffb3c5749c4bd5da1
Artem <artemef1996@gmail.com>
2021-06-27 00:47:36 +0300

    Proccesor interface added

---
+++ pcBuilder/IProccesor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pcBuilder
+{
+    interface IProccesor
+    {
+        int Frequency { get; }
+        int CoreCount { get; }
+        int TDP { get; }
+        Sockets Socket { get; }
+    }
+}
