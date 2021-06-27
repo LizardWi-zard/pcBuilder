0006ac0b3b12730cb5802e5db2ee3b99901b5437
Artem <artemef1996@gmail.com>
2021-06-27 22:43:08 +0300

    5. Added processors enum

---
+++ pcBuilder/CPUs/Processors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pcBuilder.CPUs
+{
+    enum Processors
+    {
+        CoreI3,
+        CoreI5,
+        CoreI7,
+        CoreI9,
+        Ryzen3,
+        Ryzen5,
+        Ryzen7,
+        Ryzen9,
+        Fx8350,
+        Fx8590
+    }
+}
